ff6a4020dd4b585d3a39b5bd9d7752896b996170
Nenye <chinenyediei@gmail.com>
2019-05-27 14:12:00 -0500

    gifs appear but same ones appear on different clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,4 @@
-
-
-
-//setup ajax
-var sportsTeams = ["Milwaukee Bucks", "Wisconsin Badgers", "Milwaukee Brewers", "Green Bay Packers", "Cincinnati Reds", "New York Yankees", "Miami Heat", "Los Angeles Lakers", "Ohio State Buckeyes", "Kentucky Wildcats", "UWM Panthers", "Clemson Tigers"];
+var sportsTeams = ["basketball", "football", "tennis", "soccer", "rugby", "polo", "swimming", "poker", "baseball", "volleyball", "racing"];
 
 function makeButtons()
 {
@@ -13,22 +9,96 @@ function makeButtons()
     {
         
     
-        var sportsButton = $("<button>"); 
-        sportsButton.addClass("sports"); 
+        var sportsButton = $("<button>");  
+        sportsButton.addClass("sports", sportsTeams[i]); 
         sportsButton.attr("rating");
+        sportsButton.attr("data-name", sportsTeams[i]);
         sportsButton.text(sportsTeams[i]); 
         $("#buttons").append(sportsButton); 
+      
 
         }
 
 
 }
-$("#add-sport").on("click", function (event) {
-    var add = $("#sports-input").val().trim(); 
-    sportsTeams.push(add);
+makeButtons();
+
+
+$(".sports").on("click", function () {
     event.preventDefault();
+    var sports = $(this).attr("data-name");
+    // var sports = $(".sports").val();
+    console.log(sports); 
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=sports&api_key=iUtiXYQ7vUQFZHEn1OCJ7NJayuaxQ7GK&limit=10";
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function (response)
     
-    makeButtons();
+    
+    {
+
+        
+        for (var i = 0; i < sportsTeams.length; i++) {
+                    
+            console.log(response);
+
+            // var sports = $(this).attr("rating"); 
+            //url 
+            
+            // var x = $(this).data("sports"); 
+            //create divs
+            var sportsDiv = $("<div>");
+            //create rating paragraph 
+            var p = $("p").text("Rating :" + response.data[i].rating);
+            //create image
+            var sportsImage = $("<img>");
+            sportsImage.addClass("image"); 
+            //grab image from api
+
+            sportsImage.attr("src", response.data[i].images.fixed_height.url);
+            //append p to created div
+            sportsDiv.append(p);
+            //append image to created div
+            sportsDiv.append(sportsImage);
+            //send images and p to html
+            $("#gifsdiv").prepend(sportsDiv);
+            
+        }
+        $(".img").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+    });
 });
-//call function
-makeButtons(); 
+    
+//function to add a button
+function addButton()
+{
+var addTeam = $()
+
+
+    makeButtons(); 
+}
+
+// var sports = $(this).attr("rating");
+
+
+
+
+// iUtiXYQ7vUQFZHEn1OCJ7NJayuaxQ7GK
+// http://api.giphy.com/v1/gifs/search?q=sports%20teams&api_key=iUtiXYQ7vUQFZHEn1OCJ7NJayuaxQ7GK&limit=10
